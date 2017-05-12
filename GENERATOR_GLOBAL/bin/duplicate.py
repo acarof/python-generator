cb3a4f29a36995d9f7326f08bd591869c532773c
Antoine Carof <antoine.carof@gmail.com>
2017-05-12 18:08:31 +0100

    Improve duplicate.py: copy just initial needed. Ref T175

--- GENERATOR_GLOBAL/bin/duplicate.py
+++ GENERATOR_GLOBAL/bin/duplicate.py
@@ -1,4 +1,4 @@
-import os, shutil,sys
+import os, shutil,sys, re
 
 
 try:
@@ -6,6 +6,7 @@ try:
 except:
     print "A TASK TO DUPLICATE IS REQUIRED!"
     raise SystemExit
+
 
 if 'preparation/' in task:
     taskname = task.split('/')[1]
@@ -17,15 +18,30 @@ try:
 except:
     name = 'TONAME'
 
+
 if not os.path.exists(name):
     os.mkdir(name)
 else:
     print ("%s ALREADY EXISTS" % name)
     raise SystemExit
 
+
 dst=name
-srclist = ['bin', 'templates', 'scripts','initial']
+
+srclist = ['bin', 'templates', 'scripts']
 for src in srclist:
     shutil.copytree(src, dst + '/' + src)
 
 shutil.copytree(task, name + '/' + taskname)
+
+with open('%s/task.py' % task) as file:
+    text = file.read()
+    try:
+        inits = re.findall(r"'FILE_INIT': '([^']*)", text)[0]
+        fileinit = True
+        print "Hey"
+    except:
+        fileinit = False
+if fileinit:
+   os.mkdir('%s/initial' % name)
+   os.system('cp -r initial/*-%s %s/initial/' % (inits, name))
