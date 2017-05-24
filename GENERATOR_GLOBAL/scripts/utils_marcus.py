50f71ff0011a7d81a39d0afe28c91825d58f1840
Antoine Carof <antoine.carof@gmail.com>
2017-05-24 17:27:21 +0100

    Clean marcus modules. Ref T125

--- GENERATOR_GLOBAL/scripts/utils_marcus.py
+++ GENERATOR_GLOBAL/scripts/utils_marcus.py
@@ -57,5 +57,22 @@ def calculate_boltzman_ratio( reorga, free_energy, coupling, temperature, state 
 
 
 
+def calculate_barrier(reorganization, free_energy):
+    energy_barrier = np.square( ( reorganization + free_energy ) ) / ( 4 * reorganization)
+    print energy_barrier
+    return  energy_barrier
 
+
+def calculate_marcus_na_rate(coupling, reorganization, free_energy, temperature):
+    #Convert everything in atomic units
+    coupling = coupling / 27.211399
+    reorganization = reorganization / 27.211399
+    free_energy = free_energy /     27.211399
+    temperature = temperature / 315777.09
+
+    energy_barrier = calculate_barrier(reorganization, free_energy)
+
+    rate = (2 * np.pi) * np.square(coupling) * np.exp( - energy_barrier / temperature) / np.sqrt( 4 * np.pi * reorganization * temperature)
+    rate = rate / 0.024188
+    return rate
 
