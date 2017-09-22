cf9ffddc8dd4ecbebee1c0a865a0812c9b8be8d4
antoine <antoine.carof@gmail.com>
2017-09-22 12:58:13 +0100

    Correction for farming. Ref T320

--- GENERATOR_FSSH_OS/task-for-fssh-taskfarm.sh
+++ GENERATOR_FSSH_OS/task-for-fssh-taskfarm.sh
@@ -13,6 +13,6 @@ echo -e "task $taskindex running on core $core on node $node"
 cd run-fssh-${taskindex}
 
 # Use the .sopt for FSSH
-../cp2k.sopt run.inp > run.log
+/work/e358/e358/acarof/src/nonadiabatic-cp2k/cp2k/exe/ARCHER/cp2k.sopt run.inp > run.log 2> run.err
 
 exit 0
