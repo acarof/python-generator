2fdcb445a39cd265f6acc6d673453ff9a46ce71e
Dr Antoine Carof <antoine.carof@gmail.com>
2017-10-18 15:45:21 +0100

    Ready for adiab equilib initial in ARCHER. Ref T355

--- GENERATOR_FSSH_OS/bin/check-cut.py
+++ GENERATOR_FSSH_OS/bin/check-cut.py
@@ -12,10 +12,13 @@ for dir in os.listdir('.'):
         os.system('rm %s/run-mix-1.ener' % dir)
         os.system('rm %s/run-r-1.out' % dir)
         failed = True
-        with open('%s/run.log' % dir) as logfile:
-            for line in logfile.readlines():
-                if 'PROGRAM ENDED AT' in line:
-                    failed = False
+	try:
+	        with open('%s/run.log' % dir) as logfile:
+        	    for line in logfile.readlines():
+                	if 'PROGRAM ENDED AT' in line:
+                    		failed = False
+	except:
+		pass
         if failed:
             os.system('mv %s fail/%s' % (dir, dir))
 
--- GENERATOR_FSSH_OS/task-for-fssh-taskfarm.sh
+++ GENERATOR_FSSH_OS/task-for-fssh-taskfarm.sh
@@ -5,20 +5,29 @@
 
 ./cpuid.x $CORES_PER_EXE
 core=$?
-taskindex=$(((node-1)*CORES_PER_NODE + core))
+#repeat=6
+taskindexsimple=$(( ( (node-1)*CORES_PER_NODE + core )  ))
+taskindex=$(( ( (node-1)*CORES_PER_NODE + core )*repeat  ))
 
-# Uncomment to debug
-echo -e "task $taskindex running on core $core on node $node"
 
-cd run-fssh-${taskindex}
+START_RUN=$taskindex
+LAST_RUN=$(( taskindex + repeat  - 1))
 
-# Use the .sopt for FSSH
-/work/e05/e05/acarofmc/src/CP2K/nonadiabatic-cp2k/cp2k/exe/ARCHER/cp2k.sopt run.inp > run.log &
+for (( i=$START_RUN; i<=$LAST_RUN; i++ ))
+do
+   cd run-fssh-$i
+   # Use the .sopt for FSSH
+   echo -e "task $taskindexsimple running on core $core on node $node and doing run-fssh-$i"
+   #/work/e05/e05/acarofmc/src/CP2K/nonadiabatic-cp2k/cp2k/exe/ARCHER/cp2k.sopt run.inp > run.log 
+   $CP2K_PATH run.inp > run.log 
+   cd ..
+done
 
 wait
 echo -e "task $taskindex finishes on core $core on node $node"
 
-sleep 1800
+#sleep 1800
+sleep $sleeptimehere
 #rm *restart*
 #rm run-r-1.out
 #rm run-mix-1.ener
