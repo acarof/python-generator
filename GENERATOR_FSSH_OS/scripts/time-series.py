2fc0f768010b4b1d8b559486b39c838e13c3261b
Dr Antoine Carof <antoine.carof@gmail.com>
2017-12-13 15:47:49 +0000

    Add extraction of site-energies and script to analyse one run.

---
+++ GENERATOR_FSSH_OS/scripts/time-series.py
@@ -0,0 +1,117 @@
+# standard modules
+import sys, os, time
+from multiprocessing import Pool, cpu_count
+
+
+# custom modudules
+from utils_analyse import *
+
+try:
+     name = sys.argv[1]
+except:
+     print "No directory"
+     raise SystemExit
+
+scripts = name.replace("/","")
+keywords = ['TEMPERATURE']
+dict_properties = {
+    'Runs-average' :  ['Adiabatic-populations', 'Surface-populations', 'Delta_E',
+                       'Populations', 'MSD', 'IPR',
+                       'Projected-populations', 'Projected-MSD', 'Projected-IPR',
+                       'MQC-populations', 'MQC-MSD', 'MQC-IPR', 'Total-energy',
+                       'Temperature','Off-diagonals', 'Adiabatic-energies', 'Site-energies'],
+
+}
+number_blocks = 1
+# FOR HISTO
+histo_info = {
+    'Off-diagonals' : {
+        'nbin' : 100,
+        'max_' : 0.01,
+        'min_' : -0.01
+    },
+    'Delta_E' : {
+        'nbin': 100,
+        'max_': 0.01,
+        'min_': -0.01
+    }
+}
+
+#FIND THE TITLE
+name_bucket = os.getcwd().split('/')[-1]
+short_time = time.strftime("%y%m%d%H%M", time.localtime())
+title = '%s-%s' % (name_bucket, short_time,)
+dirlist = os.listdir('.')
+dataname = 'data-%s-%s' % (scripts, title)
+if not os.path.isdir(dataname):
+    os.mkdir(dataname)
+
+
+
+# CREATE LIST OF RUNS
+run_dict = {}
+os.system('cd ..')
+#for i, directory in enumerate(dirlist):
+#    if 'run-fssh' in directory and 'per' not in directory:
+#        keys = tuple(keywords)
+#        if run_dict.get(keys ) is None:
+#            run_dict[keys] = []
+#        run_dict[keys].append(directory)
+keys = tuple(keywords)
+run_dict[keys] = [ name ]
+
+
+
+
+# PARALLEL OR SERIAL CALCULATION
+def super_analyse(tuple):
+    return analyse_properties(tuple, run_dict, dict_properties, number_blocks=number_blocks, histo_info=histo_info)
+try:
+    nworker = int( sys.argv[1] )
+except:
+    nworker = -1
+if nworker == -1:
+    nworker = cpu_count()
+if nworker == 0:
+    results = []
+    for keys in run_dict.keys():
+        results.append( super_analyse(keys) )
+else:
+    pool = Pool(nworker)
+    results = pool.map(super_analyse, run_dict.keys())
+
+
+
+# PRINT THE RESULTS
+results_dict = {}
+with open('%s/List-tuple.dat' % dataname, 'w') as filetuple:
+    filetuple.write('%s\n' % ('  '.join(keywords)))
+    for tuple, result in zip( run_dict.keys(), results):
+        filetuple.write('%s\n' % '  '.join(tuple))
+        results_dict[tuple] = result
+        properties = results_dict[tuple]
+        for method in dict_properties:
+            dict_ = properties[method]
+            for property in dict_properties[method]:
+                if method in ['Mean', 'Specific', 'Initial', 'Last']:
+                    create_file(property, title, dict_[property + 'info'], method, dataname, tuple=tuple)
+                elif method == 'Runs-average':
+                    dict_[property] = average_dict(dict_[property], properties['Number runs'])
+                    print_dict(dict_[property], property, tuple, dataname)
+                elif method == 'Block-runs-average':
+                    new_list = []
+                    for element in dict_[property]:
+                        new_list.append(average_dict(element, properties['Length-block']))
+                    print_list_dict(new_list, property, tuple, dataname)
+                elif method == 'Histogram':
+                    print_histo(property, dict_[property], dict_[property + 'bins'], dataname, tuple=tuple)
+                else:
+                    print "Unknown method: %s" % method
+                    raise SystemExit
+
+
+
+print "End of the analysis at %s" %\
+    datetime.strftime(datetime.now(), "%Y %m %d %H:%M:%S ")
+
+
--- GENERATOR_FSSH_OS/scripts/utils_scripts.py
+++ GENERATOR_FSSH_OS/scripts/utils_scripts.py
@@ -47,6 +47,8 @@ class FSSHRun(object):
             return self._extract_detailed_fssh()
         elif (property == 'Delta_E'):
             return self._extract_delta_e()
+        elif (property == 'Site-energies'):
+            return self._extract_site_energies()
         elif (property == 'State'):
             return self._extract_state()
         elif (property == 'Forces'):
@@ -401,6 +403,16 @@ class FSSHRun(object):
             for i in range(1, len(self.hamiltonians.get(time))):
                 delta_e[time].append(
                     self.hamiltonians.get(time)[i][i + 2] - self.hamiltonians.get(time)[i - 1][i - 1 + 2]
+                )
+        return delta_e
+
+    def _extract_site_energies(self, filename='run-hamilt-1.xyz'):
+        delta_e = {}
+        for time in self.hamiltonians:
+            delta_e[time] = []
+            for i in range(len(self.hamiltonians.get(time))):
+                delta_e[time].append(
+                     self.hamiltonians.get(time)[i][i + 2]
                 )
         return delta_e
 
